4f8ef5879870898bd89abc78ce05039c8274cbed
Edson Sales <edsondesouzasales@gmail.com>
2023-05-28 00:27:18 -0300

    fix:Agora o readClassFile altera a variavel na qual foi passado o endereço

--- leitores/read-class-file.c
+++ leitores/read-class-file.c
@@ -1,6 +1,6 @@
 #include "read-class-file.h"
 
-int readClassFile(char path[], ClassFile *classFileAdress){
+int readClassFile(char path[], ClassFile* classFile){
   if (!strstr(path, ".class")){
     fprintf(stderr, "Arquivo não é .class\n");
     return INVALID_ARGUMENTS;
@@ -12,27 +12,25 @@ int readClassFile(char path[], ClassFile *classFileAdress){
     return WRONG_PATH;
   }
 
-  ClassFile classFile = *classFileAdress;
-
-  classFile.magic_number = readU4(fp);
-  if(classFile.magic_number != 0xCAFEBABE) {
-    fprintf(stderr, "Magic number não bateu: %x", classFile.magic_number);
+  classFile->magic_number = readU4(fp);
+  if(classFile->magic_number != 0xCAFEBABE) {
+    fprintf(stderr, "Magic number não bateu: %x", classFile->magic_number);
     fclose(fp);
     return WRONG_MAGIC_NUMBER;
   }
 
-  classFile.minor_version = readU2(fp);
-  classFile.major_version = readU2(fp);
+  classFile->minor_version = readU2(fp);
+  classFile->major_version = readU2(fp);
 
-  printf("Versão do java: %d.%d\n", classFile.major_version, classFile.minor_version);
+  printf("Versão do java: %d.%d\n", classFile->major_version, classFile->minor_version);
 
-  classFile.constant_pool_count = readU2(fp);
-  printf("pool count: %d\n", classFile.constant_pool_count);
+  classFile->constant_pool_count = readU2(fp);
+  printf("pool count: %d\n", classFile->constant_pool_count);
 
-  classFile.constant_pool = malloc((classFile.constant_pool_count+1) * sizeof (cp_info));
+  classFile->constant_pool = malloc((classFile->constant_pool_count+1) * sizeof (cp_info));
   int i = 1;
-  cp_info *finalCPAdress = classFile.constant_pool+classFile.constant_pool_count-1;
-  for(cp_info * currentCPAdress = classFile.constant_pool; currentCPAdress < finalCPAdress; currentCPAdress++){
+  cp_info *finalCPAdress = classFile->constant_pool+classFile->constant_pool_count-1;
+  for(cp_info * currentCPAdress = classFile->constant_pool; currentCPAdress < finalCPAdress; currentCPAdress++){
     currentCPAdress->tag = readU1(fp);
     printf("index: %d, tag: %d, ", i, currentCPAdress->tag);
     i++;
@@ -130,30 +128,30 @@ int readClassFile(char path[], ClassFile *classFileAdress){
     }
   }
 
-  classFile.access_flags = readU2(fp);
-  printf("access_class: %x\n", classFile.access_flags );
+  classFile->access_flags = readU2(fp);
+  printf("access_class: %x\n", classFile->access_flags );
 
-  classFile.this_class = readU2(fp);
-  printf("this: %d\n", classFile.this_class);
+  classFile->this_class = readU2(fp);
+  printf("this: %d\n", classFile->this_class);
   
-  classFile.super_class = readU2(fp);
-  printf("super: %d\n", classFile.super_class);
+  classFile->super_class = readU2(fp);
+  printf("super: %d\n", classFile->super_class);
   
-  classFile.interfaces_count = readU2(fp);
-  printf("interface_count: %d\n", classFile.interfaces_count);
+  classFile->interfaces_count = readU2(fp);
+  printf("interface_count: %d\n", classFile->interfaces_count);
 
-  classFile.interfaces = malloc((classFile.interfaces_count) * sizeof (u2));
-  u2 *finalInterface = classFile.interfaces + classFile.interfaces_count;
-  for(u2 * currentInterface = classFile.interfaces; currentInterface < finalInterface; currentInterface ++){
+  classFile->interfaces = malloc((classFile->interfaces_count) * sizeof (u2));
+  u2 *finalInterface = classFile->interfaces + classFile->interfaces_count;
+  for(u2 * currentInterface = classFile->interfaces; currentInterface < finalInterface; currentInterface ++){
     *currentInterface = readU2(fp);
     printf("%d \n", *currentInterface);
   }
 
-  classFile.fields_count = readU2(fp);
-  printf("fields_count: %d\n", classFile.fields_count);
+  classFile->fields_count = readU2(fp);
+  printf("fields_count: %d\n", classFile->fields_count);
 
-  classFile.fields = malloc((classFile.fields_count) * sizeof (field_info));
-  for(field_info * field=classFile.fields; field<(classFile.fields+classFile.fields_count); field ++){
+  classFile->fields = malloc((classFile->fields_count) * sizeof (field_info));
+  for(field_info * field=classFile->fields; field<(classFile->fields+classFile->fields_count); field ++){
     printf("field\n");
     field->access_flags = readU2(fp);
     printf("access_flags: %x \n", field->access_flags  );
@@ -186,36 +184,38 @@ int readClassFile(char path[], ClassFile *classFileAdress){
     }
   }
 
-  classFile.methods_count = readU2(fp);
-  printf("methods_count: %d\n", classFile.methods_count);
-  classFile.methods = malloc((classFile.methods_count) * sizeof (method_info));;
-  for(int i = 0; i < classFile.methods_count; i++){
+  classFile->methods_count = readU2(fp);
+  printf("methods_count: %d\n", classFile->methods_count);
+  classFile->methods = malloc((classFile->methods_count) * sizeof (method_info));;
+  for(int i = 0; i < classFile->methods_count; i++){
     printf("method\n");
-    classFile.methods[i].access_flags = readU2(fp);
-    printf("access_flags: %x \n", classFile.methods[i].access_flags  );
+    classFile->methods[i].access_flags = readU2(fp);
+    printf("access_flags: %x \n", classFile->methods[i].access_flags  );
     
-    classFile.methods[i].name_index = readU2(fp);
-    printf("name index: %d \n", classFile.methods[i].name_index);
+    classFile->methods[i].name_index = readU2(fp);
+    printf("name index: %d \n", classFile->methods[i].name_index);
     
-    classFile.methods[i].descriptor_index = readU2(fp);
-    printf("descriptor_index: %d \n", classFile.methods[i].descriptor_index);
+    classFile->methods[i].descriptor_index = readU2(fp);
+    printf("descriptor_index: %d \n", classFile->methods[i].descriptor_index);
     // dá pra reutilizar o código de atributos
-    classFile.methods[i].attributes_count = readU2(fp);
-    printf("attributes_count: %d \n", classFile.methods[i].attributes_count);
+    classFile->methods[i].attributes_count = readU2(fp);
+    printf("attributes_count: %d \n", classFile->methods[i].attributes_count);
     
-    classFile.methods[i].attributes = malloc((classFile.methods[i].attributes_count) * sizeof (attribute_info));
+    classFile->methods[i].attributes = malloc((classFile->methods[i].attributes_count) * sizeof (attribute_info));
 
 
-    for(attribute_info * attribute = classFile.methods[i].attributes; attribute<(classFile.methods[i].attributes+classFile.methods[i].attributes_count); attribute++) {
-      attribute->attribute_name_index = readU2(fp);
-      printf("name_index: %d\n", attribute->attribute_name_index);
+    attribute_info * attributes = classFile->methods[i].attributes;
+    for(int attribute_index = 0;  attribute_index < classFile->methods[i].attributes_count; attribute_index++) {
 
-      attribute->attribute_length = readU4(fp);
-      printf("atribute length: %d\n", attribute->attribute_length);
+      attributes[attribute_index].attribute_name_index = readU2(fp);
+      printf("name_index: %d\n", attributes[attribute_index].attribute_name_index);
+
+      attributes[attribute_index].attribute_length = readU4(fp);
+      printf("atribute length: %d\n", attributes[attribute_index].attribute_length);
       
-      attribute->info = malloc((attribute->attribute_length) * sizeof (u1));
+      attributes[attribute_index].info = malloc((attributes[attribute_index].attribute_length) * sizeof (u1));
 
-      for(u1 * info = attribute->info; info<attribute->info+attribute->attribute_length; info++){
+      for(u1 * info = attributes[attribute_index].info; info<attributes[attribute_index].info+attributes[attribute_index].attribute_length; info++){
         *info = readU1(fp);
         printf("%02x ", *info);
       }
@@ -223,12 +223,12 @@ int readClassFile(char path[], ClassFile *classFileAdress){
     }
   }
 
-  classFile.attributes_count = readU2(fp);
-  printf("attributes_count: %d\n", classFile.attributes_count);
+  classFile->attributes_count = readU2(fp);
+  printf("attributes_count: %d\n", classFile->attributes_count);
 
 
-  classFile.attributes = malloc((classFile.attributes_count) * sizeof (attribute_info));
-  for(attribute_info * attribute=classFile.attributes; attribute<(classFile.attributes+classFile.attributes_count); attribute ++){
+  classFile->attributes = malloc((classFile->attributes_count) * sizeof (attribute_info));
+  for(attribute_info * attribute=classFile->attributes; attribute<(classFile->attributes+classFile->attributes_count); attribute ++){
 
     printf("attribute\n");
     attribute->attribute_name_index = readU2(fp);
